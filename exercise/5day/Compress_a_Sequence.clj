6e6e04e3ef62e7376803c0a61ad5dcd3531a1e81
junsup so <night1700@junsupui-iMac.local>
2021-05-05 22:52:10 +0900

    4Clojure 5day start

---
+++ exercise/5day/Compress_a_Sequence.clj
@@ -0,0 +1,11 @@
+;; 5일차
+;; 시퀀스 압축하기 (Compress a Sequence)
+;; url https://www.4clojure.com/problem/30
+;; 원문
+
+;; 한글
+;; 시퀀스에서 연속된 중복을 제거하는 함수를 작성하라.
+;; (= (apply str (__ "Leeeeeerrr0yyy")) "Leroy")
+;; (= (__ [1 1 2 3 3 2 2 3]) '(1 2 3 2 3))
+;; (= (__ [[1 2] [1 2] [3 4] [1 2]]) '([1 2] [3 4] [1 2]))
+;; answer
---
+++ exercise/5day/Drop_Every_Nth_Item.clj
@@ -0,0 +1,22 @@
+;; 5일차
+;; 매 n번 요소 제거하기 (Drop Every Nth Item)
+;; url https://www.4clojure.com/problem/41
+;; 원문
+
+;; 한글
+;; (fn filter-duplicate [lst] 
+;;   (loop [vecs lst result []]
+;;     (if (empty? vecs) 
+;;       result
+;;       (let [x (first vecs) y (second vecs)]
+;;         (if (= x y)
+;;           (recur (rest vecs) result)
+;;           (recur (rest vecs) (conj result x)))))))
+;; 시퀀스에서 매 n번째 요소를 제거하는 함수를 작성하라.
+(= (__ [1 2 3 4 5 6 7 8] 3) [1 2 4 5 6 7 8])
+(= (__ [:a :b :c :d :e :f] 2) [:a :c :e])
+(= (__ [1 2 3 4 5 6] 4) [1 2 3 5 6])
+
+
+
+
