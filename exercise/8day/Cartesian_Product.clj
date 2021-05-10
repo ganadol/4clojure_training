b63e13b3b8bffbeedb5811d3730e5431fbc5d9a7
junsup so <night1700@junsupui-iMac.local>
2021-05-11 00:01:15 +0900

    4Clojure 8day complete

--- exercise/8day/Cartesian_Product.clj
+++ exercise/8day/Cartesian_Product.clj
@@ -4,3 +4,65 @@
 ;; 원문
 
 ;; 한글
+;; 두집합의 데카르트 곱(Cartesian Product)을 계산하는 함수를 작성하라.
+;; 데카르트 곱 예시
+;; A = {a, b}, B = {c, d, e}
+;; A × B = {(a, c), (a, d), (a, e), (b, c), (b, d), (b, e)}
+;; A = {1, 2, 3}
+;; A × A = {(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)}
+;; (= (__ #{"ace" "king" "queen"} #{"spade" "heart" "diamond" "club"})
+;;    #{["ace" "spade"] ["king" "diamond"] ["ace" "heart"] ["queen" "club"]
+;;      ["ace" "diamond"] ["queen" "heart"] ["queen" "spade"]
+;;      ["queen" "diamond"] ["king" "club"] ["king" "heart"]
+;;      ["king" "spade"] ["ace" "club"]})
+;; (= (__ #{1 2 3} #{4 5})
+;;    #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+;; (= 300 (count (__ (into #{} (range 10))
+;;                   (into #{} (range 30)))))
+
+;; answer
+(fn [x y]
+  (let [ycnt (count x)]
+    (loop [a (seq x) b (seq y) cnt ycnt result []]
+      (if (empty? b)
+        (set result)
+        (if (= cnt 1)
+          (recur (seq x) (rest b) ycnt (conj result [(first a) (first b)]))
+          (recur (rest a) b (dec cnt) (conj result [(first a) (first b)])))))))
+
+;; answer other #(set (for [x %1 y %2] [x y]))
+
+(= ((fn [x y]
+      (let [ycnt (count x)]
+        (loop [a (seq x) b (seq y) cnt ycnt result []]
+          (if (empty? b)
+            (set result)
+            (if (= cnt 1)
+              (recur (seq x) (rest b) ycnt (conj result [(first a) (first b)]))
+              (recur (rest a) b (dec cnt) (conj result [(first a) (first b)]))))))) #{1 2 3} #{4 5})
+   #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+
+(= ((fn [x y]
+      (let [ycnt (count x)]
+        (loop [a (seq x) b (seq y) cnt ycnt result []]
+          (if (empty? b)
+            (set result)
+            (if (= cnt 1)
+              (recur (seq x) (rest b) ycnt (conj result [(first a) (first b)]))
+              (recur (rest a) b (dec cnt) (conj result [(first a) (first b)])))))))
+    #{"ace" "king" "queen"} #{"spade" "heart" "diamond" "club"})
+   #{["ace" "spade"] ["king" "diamond"] ["ace" "heart"] ["queen" "club"]
+     ["ace" "diamond"] ["queen" "heart"] ["queen" "spade"]
+     ["queen" "diamond"] ["king" "club"] ["king" "heart"]
+     ["king" "spade"] ["ace" "club"]})
+   
+(= 300 (count ((fn [x y]
+                 (let [ycnt (count x)]
+                   (loop [a (seq x) b (seq y) cnt ycnt result []]
+                     (if (empty? b)
+                       (set result)
+                       (if (= cnt 1)
+                         (recur (seq x) (rest b) ycnt (conj result [(first a) (first b)]))
+                         (recur (rest a) b (dec cnt) (conj result [(first a) (first b)])))))))
+               (into #{} (range 10))
+               (into #{} (range 30)))))
--- exercise/8day/Simple_Closures.clj
+++ exercise/8day/Simple_Closures.clj
@@ -11,6 +11,52 @@
 ;; 추상적으로 이해하는 것은 어려울 수 있으니 간단한 클로저(closure)를 만들어 보자.
 ;; 만들 함수는 양의 정수 n을 인수로 받은 후 x의 n승을 계산하는 함수 (fx)를 반환해야 한다.
 ;; 클로저(closure)로 인해 n이 정의된 영역 바깥에서도 n의 값이 보존된다는 점을 눈여겨 보자.
-(= 256 ((__ 2) 16) ((__ 8) 2))
-(= [1 8 27 64] (map (__ 3) [1 2 3 4]))
-(= [1 2 4 8 16] (map #((__ %) 2) [0 1 2 3 4]))
+;; (= 256 ((__ 2) 16) ((__ 8) 2))
+;; (= [1 8 27 64] (map (__ 3) [1 2 3 4]))
+;; (= [1 2 4 8 16] (map #((__ %) 2) [0 1 2 3 4]))
+
+;; answer
+(fn [a]
+  (partial (fn [x y]
+             (loop [result 1 cnt x n y]
+               (if (zero? cnt)
+                 result
+                 (recur (* result n) (dec cnt) n)))) a))
+;; answer 2 : partial (fn [x y] (reduce * (repeat x y)))
+
+;; answer other #(fn [x] (int (Math/pow x %)))
+  
+(= 256
+   (((fn [a]
+       (partial (fn [x y]
+                  (loop [result 1 cnt x n y]
+                    (if (zero? cnt)
+                      result
+                      (recur (* result n) (dec cnt) n)))) a)) 2) 16)
+   (((fn [a]
+       (partial (fn [x y]
+                  (loop [result 1 cnt x n y]
+                    (if (zero? cnt)
+                      result
+                      (recur (* result n) (dec cnt) n)))) a)) 8) 2))
+
+
+(= [1 8 27 64]
+   (map ((fn [a]
+           (partial (fn [x y]
+                      (loop [result 1 cnt x n y]
+                        (if (zero? cnt)
+                          result
+                          (recur (* result n) (dec cnt) n)))) a)) 3) [1 2 3 4]))
+
+(= [1 2 4 8 16]
+   (map #(((fn [a]
+             (partial (fn [x y]
+                        (loop [result 1 cnt x n y]
+                          (if (zero? cnt)
+                            result
+                            (recur (* result n) (dec cnt) n)))) a)) %) 2) [0 1 2 3 4]))
+
+(= 256 ((partial (fn [x y] (reduce * (repeat x y)))  2) 16) ((partial (fn [x y] (reduce * (repeat x y)))  8) 2))
+(= [1 8 27 64] (map (partial (fn [x y] (reduce * (repeat x y)))  3) [1 2 3 4]))
+(= [1 2 4 8 16] (map #((partial (fn [x y] (reduce * (repeat x y)))  %) 2) [0 1 2 3 4]))
