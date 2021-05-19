57adb7e73dee5f9f36f81734952aef84c9a42ca2
junsup so <night1700@junsupui-iMac.local>
2021-05-19 17:53:27 +0900

    4Clojure 11day complete

--- exercise/11day/Beauty_is_Symmetry.clj
+++ exercise/11day/Beauty_is_Symmetry.clj
@@ -7,70 +7,91 @@
 ;; 트리의 왼쪽과 오른쪽 거울상이면 이진 트리가 대칭이라고 하자.
 ;; 주어진 이진 트리가 대칭인지 아닌지를 결정하는 진위함수를 작성하라.
 ;; (트리를 어떻게 표현 했는지는 트리인가 아닌 문제를 보라)
-(= (__ '(:a (:b nil nil) (:b nil nil))) true)
+;; (= (__ '(:a (:b nil nil) (:b nil nil))) true)
 
-(= (__ '(:a (:b nil nil) nil)) false)
+;; (= (__ '(:a (:b nil nil) nil)) false)
 
-(= (__ '(:a (:b nil nil) (:c nil nil))) false)
+;; (= (__ '(:a (:b nil nil) (:c nil nil))) false)
 
-(= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+;; (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+;;         [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
+;;    true)
+
+;; (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+;;         [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
+;;    false)
+
+;; (= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+;;         [2 [3 nil [4 [6 nil nil] nil]] nil]])
+;;    false)
+;; answer 
+(fn [t]
+  (let [in-order (fn f[t] 
+                   (if (nil? t)
+                     []
+                     (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
+        t1 (in-order t)
+        t2 (reverse t1)]
+    (= t1 t2)))
+
+
+
+(= ((fn [t]
+  (let [in-order (fn f[t] 
+                   (if (nil? t)
+                     []
+                     (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
+        t1 (in-order t)
+        t2 (reverse t1)]
+    (= t1 t2))) '(:a (:b nil nil) (:b nil nil))) true)
+
+(= ((fn [t]
+  (let [in-order (fn f[t] 
+                   (if (nil? t)
+                     []
+                     (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
+        t1 (in-order t)
+        t2 (reverse t1)]
+    (= t1 t2))) '(:a (:b nil nil) nil)) false)
+
+(= ((fn [t]
+  (let [in-order (fn f[t] 
+                   (if (nil? t)
+                     []
+                     (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
+        t1 (in-order t)
+        t2 (reverse t1)]
+    (= t1 t2))) '(:a (:b nil nil) (:c nil nil))) false)
+
+(= ((fn [t]
+  (let [in-order (fn f[t] 
+                   (if (nil? t)
+                     []
+                     (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
+        t1 (in-order t)
+        t2 (reverse t1)]
+    (= t1 t2))) [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
         [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
    true)
 
-(= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+(= ((fn [t]
+  (let [in-order (fn f[t] 
+                   (if (nil? t)
+                     []
+                     (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
+        t1 (in-order t)
+        t2 (reverse t1)]
+    (= t1 t2))) [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
         [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
    false)
 
-(= (__ [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+(= ((fn [t]
+  (let [in-order (fn f[t] 
+                   (if (nil? t)
+                     []
+                     (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
+        t1 (in-order t)
+        t2 (reverse t1)]
+    (= t1 t2))) [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
         [2 [3 nil [4 [6 nil nil] nil]] nil]])
    false)
-
-
-(fn is-tree[l]
-  (let [pred (fn [f l]
-               (= true
-                  (= (count l) 3)
-                  (not= '() (f (rest l)))
-                  (not (false? (f (rest l)))))
-               )]
-    (loop [a l b false]
-      (if (empty? a)
-        b
-        (if (and (true? (pred first a)) (true? (pred second a)))
-          (if (not (nil? (second a)))
-            (recur (second a) true)
-            (recur (second (rest a)) true))
-          false)))))
-
-(defn is-tree[l]
-  (let [pred (fn [f l]
-               (= true
-                  (= (count l) 3)
-                  (not= '() (f (rest l)))
-                  (not (false? (f (rest l)))))
-               )]
-    (loop [a l b false]
-      (if (empty? a)
-        b
-        (if (and (true? (pred first a)) (true? (pred second a)))
-          (if (not (nil? (second a)))
-            (recur (second a) true)
-            (recur (second (rest a)) true))
-          false)))))
-
-
-
-
-(fn [t]
-  (let [in-order
-        (fn f[t]
-          (if (nil? t)
-            []
-            (concat (f (second t)) (vector (first t)) (f (nth t 2)))))
-        s1 (in-order t)
-        s2 (reverse s1)]
-    (= s1 s2)))
-
-
-    
-  
